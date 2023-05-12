9f12ad62634f45bbf62e68836f748a3b1a221176
vudovenko <vladislav.udovenko0@yandex.ru>
2023-05-12 18:08:10 +0500

    init commit

---
+++ src/main/java/ru/bit66/catalogfootballplayers/CatalogFootballPlayersApplication.java
@@ -0,0 +1,13 @@
+package ru.bit66.catalogfootballplayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogFootballPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogFootballPlayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bit66/catalogfootballplayers/CatalogFootballPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bit66.catalogfootballplayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogFootballPlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
