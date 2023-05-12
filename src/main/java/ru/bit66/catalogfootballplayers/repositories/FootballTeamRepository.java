c9ffe3371f68a38fb892168abdaeca3ea77856e2
vudovenko <vladislav.udovenko0@yandex.ru>
2023-05-12 18:40:10 +0500

    added the FootballTeamRepository

---
+++ src/main/java/ru/bit66/catalogfootballplayers/repositories/FootballTeamRepository.java
@@ -0,0 +1,7 @@
+package ru.bit66.catalogfootballplayers.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.bit66.catalogfootballplayers.entitites.FootballTeam;
+
+public interface FootballTeamRepository extends JpaRepository<FootballTeam, Long> {
+}
