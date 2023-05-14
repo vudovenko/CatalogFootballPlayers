9565437f41fe3ba5c6df5147411fb79b079783d6
vudovenko <vladislav.udovenko0@yandex.ru>
2023-05-14 11:30:36 +0500

    Added name and surname validation: ban on numbers

--- src/main/java/ru/bit66/catalogfootballplayers/entitites/Footballer.java
+++ src/main/java/ru/bit66/catalogfootballplayers/entitites/Footballer.java
@@ -3,6 +3,7 @@ package ru.bit66.catalogfootballplayers.entitites;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import ru.bit66.catalogfootballplayers.enums.Country;
@@ -24,9 +25,11 @@ public class Footballer {
     private Long id;
     @NotBlank
     @Size(min = 2, max = 30)
+    @Pattern(regexp = "^[^0-9]*$", message = "Имя не должно содержать цифры")
     private String firstName;
     @NotBlank
     @Size(min = 2, max = 30)
+    @Pattern(regexp = "^[^0-9]*$", message = "Имя не должно содержать цифры")
     private String lastName;
     @NotNull
     private Date birthDate;
